409dd84c063c43f295f16c17cf3be128a19535ea
manu-spherical <manuhegdev@gmail.com>
2018-04-07 18:11:42 +0530

    tiny changes

--- src/libfsort.cpp
+++ src/libfsort.cpp
@@ -2,10 +2,10 @@
 #ifndef _vec_ret
 vector<vector<float>> fvec;
 #endif
-vector<vector<double>> extract_embeddings(vector<string> image_files,int max_iters) //add option to change batch size and and crop sizes
+vector<vector<double>> extract_embeddings(vector<string> image_files,int accuracy_level) //add option to change batch size and and crop sizes
 {
     ofstream file_list;
-    tsne_wrapper tsne_obj(2,max_iters);
+    tsne_wrapper tsne_obj(2,1000);
     file_list.open("file_list.txt");
     for(vector<string>::iterator i=image_files.begin();i!=image_files.end();i++)
         file_list<<*i<<" 0\n";
