13f3f56c6486962ee4c18ea4c8602f6bcafce56e
manu-spherical <manuhegdev@gmail.com>
2018-04-19 00:37:20 +0530

    small changes in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/zsh
-pacaur -S cblas
-pacaur -S google-glog gflags lmdb hdf5 opencv2 atlas-lapack boost boost-libs ilmbase
+
+# pacaur -S cblas
+# pacaur -S google-glog gflags lmdb hdf5 opencv2 atlas-lapack boost boost-libs ilmbase
+
 #if atlast-lapack pacaur insstall doesnt work 
 #git clone https://aur.archlinux.org/atlas-lapack.git
 #cd atlas-lapack
 #nano PKGBUILD
 # add --cripple-atlas-performance to ./configure command
 #makepkg -si 
-cd /usr/lib64
-sudo ln -s libcblas.so libcblas.so.3
-sudo ln -s libIlmImf-2_2.so.23 libIlmImf-2_2.so.22
-sudo ln -s libIex-2_2.so.23 libIex-2_2.so.12
-sudo ln -s libHalf.so.23.0.0 libHalf.so.12
-sudo ln -s libIlmThread-2_2.so.23 libIlmThread-2_2.so.12
-#mkdir -p models/bvlc_googlenet
+
+# cd /usr/lib64
+# sudo ln -s libcblas.so libcblas.so.3
+# sudo ln -s libIlmImf-2_2.so.23 libIlmImf-2_2.so.22
+# sudo ln -s libIex-2_2.so.23 libIex-2_2.so.12
+# sudo ln -s libHalf.so.23.0.0 libHalf.so.12
+# sudo ln -s libIlmThread-2_2.so.23 libIlmThread-2_2.so.12
+mkdir -p models/bvlc_reference_caffenet
 cd ml_data
-wget http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
-#mv bvlc_googlenet.caffemodel models/bvlc_googlenet/
+wget http://dl.caffe.berkeleyvision.org/bvlc_reference_caffenet.caffemodel
+mv bvlc_reference_caffenet.caffemodel models/bvlc_reference_caffenet/
 mkdir -p ilsvrc12
 cd ilsvrc12
 wget -c http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz
