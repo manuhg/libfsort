db4a96097c329498be9c3e71ab04f3ea86c8b394
manu-spherical <manuhegdev@gmail.com>
2018-04-28 22:45:53 +0530

    build exec automated

--- src/build.sh
+++ src/build.sh
@@ -4,14 +4,18 @@
 #cd ../bhtsne
 #./build.sh
 #cd ../src
+set -x
 var=1
+flags=""
+exec_flag=-DBUILD_EXECUTABLE=ON
 if [[ $# -gt 0 ]]; then
     rm -rf build
     mkdir build
     cd build
     ((var=var+1))
     [ -f libfsort.a ] && rm libfsort.a
-    cmake .. && make -j8 #let make create upto 8 threads
+    [ $1 = "exec" ] && flags=$exec_flag
+    cmake .. $flags && make -j8 #let make create upto 8 threads
 else
     cd build
     ((var=var+1))
--- src/libfsort.cpp
+++ src/libfsort.cpp
@@ -37,25 +37,3 @@ vector<vector<double>> extract_embeddings(vector<string> image_files,int perplex
         display_vector_<double>(embeddings);
     return embeddings;
 }
-//#define _main
-#ifdef _main
-#include<iostream>
-#include<string.h>
-#include<cstring>
-using namespace std;
-int main(int argc, char ** argv)
-{
-    vector<string> files;
-    for(int i=1;i<argc;i++)
-    {
-        string a(argv[i],strlen(argv[i]));
-        files.push_back(a);
-        //cout<<"\n"<<a;
-    }
-    vector<vector<double>> v=extract_embeddings(files,1);
-    cout<<endl;
-    return 0;
-}
-
-
-#endif
